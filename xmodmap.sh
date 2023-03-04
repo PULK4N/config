ba97c0e6048a1f03c57f739beefcfcf072eb74c0
PULK4N <nikola_s99@live.com>
2023-03-04 14:27:17 +0100

    changed xmodmap to shell since file doesn't work

---
+++ xmodmap.sh
@@ -0,0 +1,6 @@
+xmodmap -e "remove Lock = Caps_Lock"
+xmodmap -e "keysym Caps_Lock = Control_L"
+xmodmap -e "add Lock = Caps_Lock"
+xmodmap -e "add Control = Control_L"
+xmodmap -e "keycode 66 = Multi_key"
+xmodmap -e "keysym Multi_key = Escape Escape"
