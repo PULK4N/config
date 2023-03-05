4bf683bdc1050106dde893ca7418d5b0f5d3d112
PULK4N <nikola_s99@live.com>
2023-03-06 00:29:02 +0100

    fonts added

--- scrpts/init-nerdfonts.sh
+++ scrpts/init-nerdfonts.sh
@@ -1,1 +1,2 @@
 sudo apt-get install -y fonts-hack-ttf
+cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
