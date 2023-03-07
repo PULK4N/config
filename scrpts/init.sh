347f798d5a6829cbe1ec69b7978b1d7533fd7510
PULK4N <nikola_s99@live.com>
2023-03-07 23:59:08 +0100

    coppied gihook get passwords

--- scrpts/init.sh
+++ scrpts/init.sh
@@ -1,8 +1,10 @@
 sudo apt-get update
-sudo apt-get install git
+sudo apt-get install -y git
 sudo snap install docker
 ./install-node.sh
 ./init-neovim.sh
 ./project-installs.sh
 ./xmodmap.sh
 ./init-nerdfonts.sh
+sudo apt-get install -y keepassxc
+sudo apt-get install -y fzf
---
+++ scrpts/non-install/gihook-get-pass.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+source ~/.secrets/environment-variables
+
+file_path=$(echo $MY_PWD_FILE_LOCATION)
+
+selected=$(echo $MY_KEEPASS | keepassxc-cli ls $file_path | fzf)
+
+timeout_in_seconds=5
+
+echo $MY_KEEPASS | keepassxc-cli clip "$file_path" "$selected" $timeout_in_seconds
